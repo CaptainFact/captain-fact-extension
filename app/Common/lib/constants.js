0a776e5e2f1aafce35728663047cee03b25ac0ab
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-09-28 23:44:42 +1100

    Minor UI fixes, upgrade to 0.4.2 & update README examples

--- app/Common/lib/constants.js
+++ app/Common/lib/constants.js
@@ -2,4 +2,4 @@ import {FRONT_URL, API_URL} from 'config'
 
 export const CF_FRONT_URL = FRONT_URL
 export const CF_API_URL = API_URL
-export const STATEMENT_FOCUS_TIME = 15
+export const STATEMENT_FOCUS_TIME = 30
