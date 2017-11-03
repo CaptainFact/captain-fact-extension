f50cd8b0e981ac5da77f5c1837d5e24adb737bb0
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2017-11-04 09:31:12 +1100

    Show mini picture instead of picture for users

--- app/InjectVideo/Fact/Fact.js
+++ app/InjectVideo/Fact/Fact.js
@@ -2,15 +2,14 @@ import React from 'react'
 import Source from './Source.js'
 import UserAppellation from './UserAppellation'
 import { fact, approvingFact, refutingFact, regularFact, userSection, right, scoreTag, sourceSection, userCommentText } from './Fact.css'
+import { staticResource } from '../../Common/lib/static_resource'
 import LocalImage from '../Utils/LocalImage'
 
 
-const getUserPicture = (userId, url, size) => {
-  if (!url)
+const getUserPicture = (userId, url, url_mini, size) => {
+  if (!url || !url_mini)
     return `https://api.adorable.io/avatars/${size}/${userId}.png`
-  if (url.includes('facebook.com'))
-    return url.split('?')[0] + `?width=${size}&height=${size}`
-  return url
+  return size <= 48 ? staticResource(url_mini) : staticResource(url)
 }
 
 export default class Fact extends React.PureComponent {
@@ -27,7 +26,7 @@ export default class Fact extends React.PureComponent {
         </div>
         {text !== null && text.length > 0 &&
           <div className={userSection}>
-            <img src={getUserPicture(user.id, user.picture_url, 32)} height="28"/>
+            <img src={getUserPicture(user.id, user.picture_url, user.mini_picture_url, 32)} height="28"/>
             <div className={right}>
               <UserAppellation user={user}/>&nbsp;
               <span className={userCommentText}>{text}</span>
