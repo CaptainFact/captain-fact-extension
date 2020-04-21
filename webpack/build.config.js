800fceb695ac2bb4341539a7c7f60fdb519ccf06
Benjamin Piouffle <benjamin.piouffle@gmail.com>
2020-04-21 20:50:58 +0200

    Don't minify code in build

--- webpack/build.config.js
+++ webpack/build.config.js
@@ -1,63 +1,57 @@
-const path = require("path");
-const webpack = require("webpack");
-const postCSSConfig = require("./postcss.config");
-const argv = require("yargs").argv;
+const path = require('path')
+const webpack = require('webpack')
+const postCSSConfig = require('./postcss.config')
+const argv = require('yargs').argv
 
 module.exports = {
   entry: {
-    popup: path.join(__dirname, "../chrome/extension/popup"),
-    background: path.join(__dirname, "../chrome/extension/background"),
-    inject: path.join(__dirname, "../chrome/extension/inject"),
+    popup: path.join(__dirname, '../chrome/extension/popup'),
+    background: path.join(__dirname, '../chrome/extension/background'),
+    inject: path.join(__dirname, '../chrome/extension/inject'),
     installation_notifier: path.join(
       __dirname,
-      "../chrome/extension/installation_notifier"
+      '../chrome/extension/installation_notifier'
     ),
   },
   output: {
-    path: path.join(__dirname, "../build/js"),
-    filename: "[name].bundle.js",
-    chunkFilename: "[id].chunk.js",
+    path: path.join(__dirname, '../build/js'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].chunk.js',
   },
   postcss() {
-    return postCSSConfig;
+    return postCSSConfig
   },
   plugins: [
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.IgnorePlugin(/[^/]+\/[\S]+.dev$/),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-      compressor: {
-        warnings: false,
-      },
-    }),
     new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify("production"),
+      'process.env': {
+        NODE_ENV: JSON.stringify('production'),
       },
     }),
   ],
   resolve: {
-    extensions: ["", ".js", ".jsx"],
+    extensions: ['', '.js', '.jsx'],
     alias: {
-      config: path.join(__dirname, "../config/" + argv.env + ".js"),
+      config: path.join(__dirname, '../config/' + argv.env + '.js'),
     },
   },
   module: {
     loaders: [
       {
         test: /\.jsx?$/,
-        loader: "babel",
+        loader: 'babel',
         exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         loaders: [
-          "style",
-          "css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]",
-          "postcss",
+          'style',
+          'css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]',
+          'postcss',
         ],
       },
     ],
   },
-};
+}
